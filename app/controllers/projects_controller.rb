47ed6bf6c0ea264fac6bbfaa9d3bde4a6bf01a36
talaverac <talaveracp@gmail.com>
2016-11-09 15:22:45 -0600

    Add title functionality for show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,7 +12,7 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
 
     if @project.save
-      flash[:notice] = "Project has been created."
+      flash[:notice] = "Project has been created"
       redirect_to @project
     else
       # nothing, yet
